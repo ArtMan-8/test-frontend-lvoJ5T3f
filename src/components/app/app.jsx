0fe0290e3ae07dee1e407a3201d4f1533d354706
Vladimir Artyukhov <ya.Artriches@ya.ru>
2020-10-03 17:13:05 +0300

    add: app - разметка - стили - стейт - обработчик

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import Settings from '../settings/settings.jsx';
+import Widget from '../widget/widget.jsx';
+import './style.css';
 
 export default function App() {
-  return (
-    <Settings />
+  const [minNumber, setMinNumber] = React.useState(0);
+  const [maxNumber, setMaxNumber] = React.useState(100);
+  const [currentNumber, setCurrentNumber] = React.useState(50);
+
+  const widgetNumberChangeHandler = (number) => (
+    (number <= maxNumber && number >= minNumber) && setCurrentNumber(number)
   );
+
+  return <>
+    <Settings
+      currentNumber={currentNumber}
+      minNumber={minNumber}
+      maxNumber={maxNumber}
+      setCurrentNumber={widgetNumberChangeHandler}
+      setMinNumber={setMinNumber}
+      setMaxNumber={setMaxNumber}
+    />
+
+    <Widget
+      currentNumber={currentNumber}
+      minNumber={minNumber}
+      maxNumber={maxNumber}
+      onChangeNumber={widgetNumberChangeHandler}
+    />
+  </>;
 }
