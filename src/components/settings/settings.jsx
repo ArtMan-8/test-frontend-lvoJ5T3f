26a93cfe405aaa19818455055c7393db2518f2bc
Vladimir Artyukhov <ya.Artriches@ya.ru>
2020-10-03 17:07:50 +0300

    add: компанент настрек - разметка - стили - обработчики

--- src/components/settings/settings.jsx
+++ src/components/settings/settings.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Field from '../field/field.jsx';
 import style from './style.css';
 
-export default function Settings() {
+export default function Settings({
+  minNumber, maxNumber, currentNumber, setCurrentNumber, setMinNumber, setMaxNumber,
+}) {
+  const setMinNumberHandler = (number) => number <= currentNumber && setMinNumber(number);
+  const setMaxNumberHandler = (number) => number >= currentNumber && setMaxNumber(number);
+
   return <section className={style.settings}>
-    <Field title="Начальное значение" />
-    <Field title="Минимальное значение" />
-    <Field title="Максимальное значение" />
+    <Field title="Начальное значение"
+      currentNumber={currentNumber}
+      onChangeNumber={setCurrentNumber}
+    />
+
+    <Field title="Минимальное значение"
+      currentNumber={minNumber}
+      onChangeNumber={setMinNumberHandler}
+    />
+
+    <Field title="Максимальное значение"
+      currentNumber={maxNumber}
+      onChangeNumber={setMaxNumberHandler}
+    />
   </section>;
 }
+
+Settings.propTypes = {
+  currentNumber: PropTypes.number.isRequired,
+  minNumber: PropTypes.number.isRequired,
+  maxNumber: PropTypes.number.isRequired,
+  setCurrentNumber: PropTypes.func.isRequired,
+  setMinNumber: PropTypes.func.isRequired,
+  setMaxNumber: PropTypes.func.isRequired,
+};
